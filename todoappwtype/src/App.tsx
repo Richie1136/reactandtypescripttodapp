8560472c22b68d162991239c7e16724bb8ed8a7d
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-03-23 10:47:44 -0400

    Added a props interface for my two props that i passed down from the app component

--- todoappwtype/src/App.tsx
+++ todoappwtype/src/App.tsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import InputField from "./components/inputfield/InputField";
+import { useState } from "react";
 
 function App() {
+  const [todo, setTodo] = useState<string>("");
+
   return (
     <div className="App">
       <h2>Todo with React</h2>
       <span className="heading">Tasks</span>
-      <InputField />
+      <InputField todo={todo} setTodo={setTodo} />
     </div>
   );
 }
--- todoappwtype/src/components/inputfield/InputField.tsx
+++ todoappwtype/src/components/inputfield/InputField.tsx
@@ -1,6 +1,12 @@
+import React from "react";
 import "./InputField.css";
 
-const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField = ({ todo, setTodo }: Props) => {
   return (
     <form className="input">
       <input className="input-box" type="text" placeholder="Enter a Task" />
