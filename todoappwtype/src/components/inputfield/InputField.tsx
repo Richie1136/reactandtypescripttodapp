992ac0ad329bc0a2630cffaad5db1ff024d504ee
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-03-23 10:57:08 -0400

    Added a handleSubmit function which will prevent the default behavior of reloading the page and then i added the event type of React.FormEvent<HTMLFormElement> and then added that function as a onSubmit to my form element

--- todoappwtype/src/components/inputfield/InputField.tsx
+++ todoappwtype/src/components/inputfield/InputField.tsx
@@ -10,8 +10,13 @@ const InputField = ({ todo, setTodo }: Props) => {
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTodo(event.target.value);
   };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event?.preventDefault();
+  };
+
   return (
-    <form className="input">
+    <form onSubmit={handleSubmit} className="input">
       <input className="input-box" type="text" onChange={handleChange} value={todo} placeholder="Enter a Task" />
       <button className="submit" type="submit">
         Submit
