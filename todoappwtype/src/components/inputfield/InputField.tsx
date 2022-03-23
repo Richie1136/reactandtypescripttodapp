fd20d926cc154dce23777d4c75fc4cade66e8b13
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-03-23 10:39:02 -0400

    Qdded styling for the input field and the submit button as well as added a input field and submit button

--- todoappwtype/src/components/inputfield/InputField.tsx
+++ todoappwtype/src/components/inputfield/InputField.tsx
@@ -1,5 +1,14 @@
+import "./InputField.css";
+
 const InputField = () => {
-  return <div>InputField</div>;
+  return (
+    <form className="input">
+      <input className="input-box" type="text" placeholder="Enter a Task" />
+      <button className="submit" type="submit">
+        Submit
+      </button>
+    </form>
+  );
 };
 
 export default InputField;
