0bfe8504611f7150013688c988763b593d8b295d
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-03-23 10:21:45 -0400

    Created a input field component and added it inside of the app.tsx file

--- todoappwtype/src/App.tsx
+++ todoappwtype/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import InputField from "./components/inputfield/InputField";
 
 function App() {
   return (
     <div className="App">
       <h2>Todo with React</h2>
       <span className="heading">Tasks</span>
+      <InputField />
     </div>
   );
 }
