5b2c6fc69d2fd2b1f28c9ef47c0dc0613d45ebdd
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-03-24 14:52:56 -0400

    Adding todos and using Date.now() as a unique id for each todo object

--- todoappwtype/src/App.tsx
+++ todoappwtype/src/App.tsx
@@ -7,11 +7,21 @@ function App() {
   const [todo, setTodo] = useState<string>("");
   const [todos, setTodos] = useState<Todo[]>([]);
 
+  const handleAdd = (event: React.FormEvent<HTMLFormElement>) => {
+    event?.preventDefault();
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
+      setTodo("");
+    }
+  };
+
+  console.log(todos);
+
   return (
     <div className="App">
       <h2>Todo with React</h2>
       <span className="heading">Tasks</span>
-      <InputField todo={todo} setTodo={setTodo} />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
     </div>
   );
 }
--- todoappwtype/src/components/inputfield/InputField.tsx
+++ todoappwtype/src/components/inputfield/InputField.tsx
@@ -4,19 +4,16 @@ import "./InputField.css";
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (event: React.FormEvent<HTMLFormElement>) => void;
 }
 
-const InputField = ({ todo, setTodo }: Props) => {
+const InputField = ({ todo, setTodo, handleAdd }: Props) => {
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTodo(event.target.value);
   };
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event?.preventDefault();
-  };
-
   return (
-    <form onSubmit={handleSubmit} className="input">
+    <form onSubmit={handleAdd} className="input">
       <input className="input-box" type="text" onChange={handleChange} value={todo} placeholder="Enter a Task" />
       <button className="submit" type="submit">
         Submit
