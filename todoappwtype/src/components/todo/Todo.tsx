7b3bdd5d71abfdce5dd44cb22eb21987de318a58
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-03-31 10:56:03 -0400

    Working on the edit functionality

--- todoappwtype/src/components/todo/Todo.tsx
+++ todoappwtype/src/components/todo/Todo.tsx
@@ -1,6 +1,7 @@
 import { AiFillDelete, AiFillEdit } from "react-icons/ai";
 import { MdOutlineDownloadDone } from "react-icons/md";
 import { Todo } from "../../interface";
+import { useState } from "react";
 import "./Todo.css";
 
 interface Props {
@@ -10,6 +11,9 @@ interface Props {
 }
 
 const ToDo = ({ todo, todos, setTodos }: Props) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editToDo, setEditToDo] = useState<string>(todo.todo);
+
   const handleDone = (id: number) => {
     setTodos(todos.map((todo) => (todo.id === id ? { ...todo, isDone: !todo.isDone } : todo)));
   };
@@ -18,16 +22,28 @@ const ToDo = ({ todo, todos, setTodos }: Props) => {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
 
+  const handleEdit = () => {
+    if (!edit && !todo.isDone) {
+      setEdit(!edit);
+    }
+  };
+
+  const setTodoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEditToDo(event?.target.value);
+  };
+
   return (
     <>
       <form className="todos-single">
+        {edit && <input className="todos-single---text" value={editToDo} onChange={setTodoChange} />}
+
         {todo.isDone ? (
           <s className="todos-single_text">{todo.todo}</s>
         ) : (
           <span className="todos-single_text">{todo.todo}</span>
         )}
         <div>
-          <span className="icon">
+          <span className="icon" onClick={handleEdit}>
             <AiFillEdit />
           </span>
           <span className="icon" onClick={() => handleDone(todo.id)}>
