deb5b25eafc68d41cc3904fa9c6b6b4e6ae15f0e
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-03-31 10:39:54 -0400

    Added functionality to delete the todo and also added functionality to strike the todo if it is completed

--- todoappwtype/src/components/todo/Todo.tsx
+++ todoappwtype/src/components/todo/Todo.tsx
@@ -1,7 +1,7 @@
-import { AiFillDelete } from "react-icons/ai";
-import { AiFillEdit } from "react-icons/ai";
+import { AiFillDelete, AiFillEdit } from "react-icons/ai";
 import { MdOutlineDownloadDone } from "react-icons/md";
 import { Todo } from "../../interface";
+import "./Todo.css";
 
 interface Props {
   todo: Todo;
@@ -10,13 +10,33 @@ interface Props {
 }
 
 const ToDo = ({ todo, todos, setTodos }: Props) => {
+  const handleDone = (id: number) => {
+    setTodos(todos.map((todo) => (todo.id === id ? { ...todo, isDone: !todo.isDone } : todo)));
+  };
+
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
     <>
       <form className="todos-single">
-        <span className="todos-signle_text">{todo.todo}</span>
-        <AiFillDelete />
-        <AiFillEdit />
-        <MdOutlineDownloadDone />
+        {todo.isDone ? (
+          <s className="todos-single_text">{todo.todo}</s>
+        ) : (
+          <span className="todos-single_text">{todo.todo}</span>
+        )}
+        <div>
+          <span className="icon">
+            <AiFillEdit />
+          </span>
+          <span className="icon" onClick={() => handleDone(todo.id)}>
+            <MdOutlineDownloadDone />
+          </span>
+          <span className="icon" onClick={() => handleDelete(todo.id)}>
+            <AiFillDelete />
+          </span>
+        </div>
       </form>
     </>
   );
