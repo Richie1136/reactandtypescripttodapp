bb08f2697cf79f3272ec775c4e4e3a755e8af15c
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-04-06 10:54:02 -0400

    Using the destination qnd the source which we get from the result and wrote conditonal statements which will remove the item from the source and then add that item to the destination and then i update my completedTodos state and my Todos state to be able to drag the item from the active tasks list to my completed tasks list

--- todoappwtype/src/App.tsx
+++ todoappwtype/src/App.tsx
@@ -3,7 +3,7 @@ import InputField from "./components/inputfield/InputField";
 import { useState } from "react";
 import { Todo } from "./interface";
 import ToDoList from "./components/todolist/ToDoList";
-import { DragDropContext } from "react-beautiful-dnd";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
 
 function App() {
   const [todo, setTodo] = useState<string>("");
@@ -18,8 +18,36 @@ function App() {
     }
   };
 
+  const onDragEnd = (result: DropResult) => {
+    const { source, destination } = result;
+
+    if (!destination) return;
+
+    if (destination.droppableId === source.droppableId && destination.index === source.index) return;
+
+    let add;
+    let active = todos;
+    let complete = completedTodos;
+
+    if (source.droppableId === "TodosList") {
+      add = active[source.index];
+      active.splice(source.index, 1);
+    } else {
+      add = complete[source.index];
+      complete.splice(source.index, 1);
+    }
+
+    if (destination.droppableId === "TodosList") {
+      active.splice(destination.index, 0, add);
+    } else {
+      complete.splice(destination.index, 0, add);
+    }
+    setCompletedTodos(complete);
+    setTodos(active);
+  };
+
   return (
-    <DragDropContext onDragEnd={() => {}}>
+    <DragDropContext onDragEnd={onDragEnd}>
       <div className="App">
         <h2>Todo with React</h2>
         <span className="heading">Tasks</span>
